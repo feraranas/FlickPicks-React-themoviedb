368e213ed0895dbe44f3e1c95e5c1bbe65877b60
feraranas <46a12sFAASfaas>
2023-06-14 13:08:42 -0600

    Fixed UX for myFavorites

--- src/pages/MyFavorites/MyFavorites.tsx
+++ src/pages/MyFavorites/MyFavorites.tsx
@@ -1,28 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShowsSlider, 
-ShowsTitle,
-ShowsSliderContent,
-LabelWithTumbs, } from './styles'
-import { MovieContext } from 'contexts/MovieContext';
 import { MovieCard } from 'components/MovieCard';
+import React, {useContext, useEffect, useState} from 'react'
+import {
+  App,
+  BodyWrapper,
+  Header,
+  ShowsTitle,
+  Movies,
+  Titulo,
+  DivTitulo,
+  Botones,
+  SortByName,
+  MovieSlider,
+  SortByCalification,
+} from "./styles";
+import SortByAlphaIcon from '@mui/icons-material/SortByAlpha';
+import SortIcon from '@mui/icons-material/Sort';
+import { MovieContext } from 'contexts/MovieContext';
 import { getMovieId } from 'services/movies/getMovies';
 
-const MyFavorites = () => {
-  const { favoriteMovies } = useContext(MovieContext);
-  const [movieAPICalls, setMovieAPICalls] = useState<any[]>([]);
 
+const MyFavorites = () => {
+  // --------------------------------------------- CONTEXT
+  const { favoriteMovies } = useContext(MovieContext);
+
+  // --------------------------------------------- STATES
+  const [movieAPICalls, setMovieAPICalls] = useState<any[]>([]);
+  const [favoriteMoviesIds, setFavoriteMoviesIds] = useState<any[]>([]);
+
+  // --------------------------------------------- USE EFFECT
   useEffect(() => {
-    console.log(favoriteMovies);
     const getMoviesFromIds = async () => {
-      
-      for (const id of favoriteMovies) {
-        try {
-          const res = await getMovieId(id);
-          if (res && res.data) {
-            setMovieAPICalls((prevMovie) => [...prevMovie, res.data]);
+      for (const _id of favoriteMovies) {
+        if (!favoriteMoviesIds.includes(_id)) {
+          try {
+            const res = await getMovieId(_id);
+            if (res && res.data) {
+              setFavoriteMoviesIds((prevMovie) => [...prevMovie, _id]);
+              setMovieAPICalls((prevMovie) => [...prevMovie, res.data]);
+            }
+          } catch (err) {
+            console.log(err, 'err');
           }
-        } catch (err) {
-          console.log(err, 'err');
         }
       }
     };
@@ -30,34 +48,43 @@ const MyFavorites = () => {
     getMoviesFromIds();
   }, [favoriteMovies]);
 
-  
-
+  // --------------------------------------------- MAIN RENDER
   return (
-    <ShowsSlider>
-      <ShowsTitle>my favorites</ShowsTitle>
-      <ShowsSliderContent>
-        <LabelWithTumbs>
-        {movieAPICalls?.length > 0 ? (
-                movieAPICalls.map((movie) => (
-                  <MovieCard
-                    key={movie.id}
-                    isAdult={movie.adult}
-                    path={movie.poster_path}
-                    title={movie.title}
-                    voteAverage={movie.vote_average}
-                    genreId={Object.values(movie.genres)}
-                    movieId={movie.id}
-                    releaseDate={movie.release_date}
-                    voteCount={movie.vote_count}
-                    description={movie.overview}
-                  />
-                ))
-                .filter((movie) => movie !== undefined)
-        ) : (<div>Fetching</div>)}
-        </LabelWithTumbs>
-      </ShowsSliderContent>
-    </ShowsSlider>
+    <App>
+      <BodyWrapper>
+        <Header>
+          <ShowsTitle>MY FAVORITES</ShowsTitle>
+          <Botones>
+            <SortByName><SortByAlphaIcon fontSize='small' />Sort by Name</SortByName>
+            <SortByCalification><SortIcon fontSize='small' />Sort by Calification</SortByCalification>
+          </Botones>
+        </Header>
+        <Movies>
+          <MovieSlider>
+            {movieAPICalls?.length > 0 ? (
+              movieAPICalls.map((movie) => (
+                <MovieCard
+                        key={movie.id}
+                        path={movie.poster_path}
+                        isAdult={movie.adult}
+                        title={movie.title}
+                        voteAverage={movie.vote_average}
+                        genreId={Object.values(movie.genre_ids)}
+                        movieId={movie.id}
+                        releaseDate={movie.release_date}
+                        voteCount={movie.vote_count}
+                        description={movie.overview}
+                />
+              ))
+            ) : (
+              <div>Cargando</div>
+            )}
+          </MovieSlider>
+        </Movies>
+      </BodyWrapper>
+    </App>
   )
 }
 
 export default MyFavorites
+
